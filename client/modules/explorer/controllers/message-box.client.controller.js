f9603de2a02437d86615b55581a5c4c1fd5c3cae
Andrey <ultra-777@yandex.ru>
2014-08-26 02:07:51 +0400

    'messagebox'

---
+++ client/modules/explorer/controllers/message-box.client.controller.js
@@ -0,0 +1,16 @@
+'use strict';
+
+angular.module('explorer').controller('MessageBoxController', ['$scope', '$modalInstance', 'model',
+    function(scope, modalInstance, model) {
+
+        scope.dialogOptions = model;
+
+        scope.ok = function () {
+            modalInstance.close();
+        };
+
+        scope.cancel = function () {
+            modalInstance.dismiss('cancel');
+        };
+    }
+]);
---
+++ client/modules/explorer/services/message-box-service.js
@@ -0,0 +1,3 @@
+/**
+ * Created by Andrey on 25.08.2014.
+ */
