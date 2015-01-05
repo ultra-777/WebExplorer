8bca317ecbb418ed887d11e4629b3d24fc045d4e
Andrey <ultra-777@yandex.ru>
2015-01-05 12:36:16 +0300

    cluster node recover

--- config/env/development.js
+++ config/env/development.js
@@ -4,7 +4,7 @@ module.exports = {
 	db: 'postgres://postgres:password@localhost:5432/site',
 	maxNodesCount: 1,
 	workerStatePeriod: 1000 * 60 * 60 * 24, // 24 hr
-	workerStateCheckTimeout: 1000 * 1, // 5 sec
+	workerStateCheckTimeout: 1000 * 60, // 1 min
 	workerStateOverheadRatio: 10,
 	app: {
 		title: 'Cheers - Development'
